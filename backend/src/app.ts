5c1e12d8e94d46689aac5202245e08938415ac74
deadsoulartist <deadsoulartist@gmail.com>
2023-07-22 11:55:10 +0530

    fix: replaced with local to test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: "https://notable-app-new-fe.onrender.com",
+  origin: "http://localhost:3000",
 };
 
 //HTTP request logger middleware for node.js
