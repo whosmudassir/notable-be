53befecc1f606b01c60ca633234d264d093e52e0
deadsoulartist <deadsoulartist@gmail.com>
2023-07-22 13:34:06 +0530

    fix: cors setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://notable-app-new-fe.onrender.com"],
+  origin: ["http://localhost:3000", "http://notable-app-new-fe.onrender.com"],
 };
 
 //HTTP request logger middleware for node.js
